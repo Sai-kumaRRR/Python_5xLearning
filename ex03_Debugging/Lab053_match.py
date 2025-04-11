7d09902e9259581b7e73b58c2e1fda4f5ea392d0
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-04-11 21:16:59 +0530

    11th April 2025

---
+++ ex03_Debugging/Lab053_match.py
@@ -0,0 +1,11 @@
+# match
+# similar to switch
+number = int(input("enter to number\n"))
+match number:
+    case 1:
+        print("you have entered 1")
+    case 2:
+
+        print("you have entered 2")
+    case _: # nothing is matching i will run
+        print("no idea")
