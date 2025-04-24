3331b21b13b2bfdf7877fd32f3658561fe3c9934
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-04-24 12:09:52 +0530

    24th April 2025

---
+++ Lab105_OOPs_03.py
@@ -0,0 +1,12 @@
+# OOPs
+#without OOPs
+# 1.) Duplicate Code
+# 2.) NO Reuse ability
+# 3.) Maintain
+# 4.)  write a program to do something
+print("Hello World")
+print("Hello Sai")
+print("Hello world 1")
+print("Hello World 2")
+print("Hello")
+
