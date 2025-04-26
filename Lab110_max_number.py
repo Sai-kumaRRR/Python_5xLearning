56df69c966d4f99ecd181977faacbc42f07a3429
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-04-26 17:29:48 +0530

    26th April 2025

---
+++ Lab110_max_number.py
@@ -0,0 +1,10 @@
+#   5 , 8 , 6 ->   8
+
+result = max(5 , 6 , 8)
+result_max = max(5 , 6 , 8)
+print(result_max)
+print(result)
+
+
+result = max(3 , 6 , 8 , 00000 , 765 , 21)
+print(result)
---
+++ Lab111_min_number.py
@@ -0,0 +1,10 @@
+#   5 , 8 , 6 ->   8
+
+result = min(5 , 6 , 8)
+result_min = max(5 , 6 , 8)
+print(result_min)
+print(result)
+
+
+result = min(3 , 6 , 8 , 00000 , 765 , 21)
+print(result)
