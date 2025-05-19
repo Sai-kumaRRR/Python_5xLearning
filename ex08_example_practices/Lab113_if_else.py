a7b753395a02ccb98d9c4e104f13f2aeef6ed4da
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-05-19 17:21:15 +0530

    04th May 2025

---
+++ ex08_example_practices/Lab113_if_else.py
@@ -0,0 +1,12 @@
+
+
+
+def minimum(first,second):
+    if(first < second):
+        print(first)
+    else:
+        print(second)
+def minimum(first, second):
+    if(first < second):
+        return first
+    return second
